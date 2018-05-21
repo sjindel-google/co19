b9fd1645815fe605f2d9dbf10675f5b77a9cd777
Natalya Golovleva <ngl@unipro.ru>
2018-05-21 18:31:55 +0700

    Tests description impoved

--- LanguageFeatures/Subtyping/generated/right_top_class_member_A03_t01.dart
+++ LanguageFeatures/Subtyping/generated/right_top_class_member_A03_t01.dart
@@ -7,7 +7,7 @@
  * @assertion We say that a type T0 is a subtype of a type T1 (written T0 <: T1)
  * when:
  * Right Top: T1 is a top type (i.e. Object, dynamic, or void)
- * @description Check that if type T1 is a dynamic then instance of
+ * @description Check that if type T1 is a void then instance of
  * T0 can be assigned to the T1 variable.
  * @author sgrekhov@unipro.ru
  */
