9437b7afdaf3f182e7a349c7b02268578ae84457
Sergey G. Grekhov <sgrekhov@unipro.ru>
2018-05-21 16:55:56 +0700

    Subtyping. Generated tests for right top added

---
+++ LanguageFeatures/Subtyping/generated/right_top_return_value_A02_t01.dart
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion We say that a type T0 is a subtype of a type T1 (written T0 <: T1)
+ * when:
+ * Right Top: T1 is a top type (i.e. Object, dynamic, or void)
+ * @description Check that if type T1 is a dynamic then instance of
+ * T0 can be assigned to the T1 variable.
+ * @author sgrekhov@unipro.ru
+ */
+
+import "../utils/common.dart";
+
+class T0 {}
+
+T0 t0Instance = new T0();
+dynamic t1Instance = null;
+
+
+
+
+dynamic returnValueFunc() => forgetType(t0Instance);
+
+class ReturnValueTest {
+  static dynamic staticTestMethod() => forgetType(t0Instance);
+
+  dynamic testMethod() => forgetType(t0Instance);
+
+  dynamic get testGetter => forgetType(t0Instance);
+}
+
+class ReturnValueGen<X> {
+  X testMethod() => forgetType(t0Instance);
+  X get testGetter => forgetType(t0Instance);
+}
+
+
+main() {
+  dynamic returnValueLocalFunc() => forgetType(t0Instance);
+
+  returnValueFunc();
+  returnValueLocalFunc();
+
+  ReturnValueTest.staticTestMethod();
+
+  new ReturnValueTest().testMethod();
+  new ReturnValueTest().testGetter;
+
+  new ReturnValueGen<T0>().testMethod();
+  new ReturnValueGen<T0>().testGetter;
+}
