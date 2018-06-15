c4d7b9c79f2679f9a5627222f0832a1c32e9aa97
Natalya Golovleva <ngl@unipro.ru>
2018-06-15 19:23:02 +0700

    New tests added.

---
+++ LanguageFeatures/Subtyping/generated/right_FutureOr_arguments_binding_A04_t01.dart
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion We say that a type T0 is a subtype of a type T1 (written T0 <: T1)
+ * when:
+ * Right FutureOr: T1 is FutureOr<S1> and
+ *   either T0 <: Future<S1>
+ *   or T0 <: S1
+ *   or T0 is X0 and X0 has bound S0 and S0 <: T1
+ *   or T0 is X0 & S0 and S0 <: T1
+ * @description Check that if a type T1 is S1 and a type T0 is X0 and X0 has
+ * bound S0 and S0 <: S1, then a type T0 is a subtype of a type T1.
+ * @author ngl@unipro.ru
+ */
+
+import "../utils/common.dart";
+
+class S1 {}
+class S0 extends S1 {
+}
+class X0 extends S0 {
+}
+
+X0 t0Instance = new X0();
+S1 t1Instance = new S1();
+
+
+
+
+namedArgumentsFunc1(S1 t1, {S1 t2}) {}
+positionalArgumentsFunc1(S1 t1, [S1 t2]) {}
+
+namedArgumentsFunc2<X>(X t1, {X t2}) {}
+positionalArgumentsFunc2<X>(X t1, [X t2]) {}
+
+class ArgumentsBindingClass {
+  ArgumentsBindingClass(S1 t1) {}
+
+  ArgumentsBindingClass.named(S1 t1, {S1 t2}) {}
+  ArgumentsBindingClass.positional(S1 t1, [S1 t2]) {}
+
+  factory ArgumentsBindingClass.fNamed(S1 t1, {S1 t2}) {
+    return new ArgumentsBindingClass.named(t1, t2: t2);
+  }
+  factory ArgumentsBindingClass.fPositional(S1 t1, [S1 t2]) {
+    return new ArgumentsBindingClass.positional(t1, t2);
+  }
+
+  static namedArgumentsStaticMethod(S1 t1, {S1 t2}) {}
+  static positionalArgumentsStaticMethod(S1 t1, [S1 t2]) {}
+
+  namedArgumentsMethod(S1 t1, {S1 t2}) {}
+  positionalArgumentsMethod(S1 t1, [S1 t2]) {}
+
+  set testSetter(S1 val) {}
+}
+
+class ArgumentsBindingGen<X>  {
+  ArgumentsBindingGen(X t1) {}
+
+  ArgumentsBindingGen.named(X t1, {X t2}) {}
+  ArgumentsBindingGen.positional(X t1, [X t2]) {}
+
+  factory ArgumentsBindingGen.fNamed(X t1, {X t2}) {
+    return new ArgumentsBindingGen.named(t1, t2: t2);
+  }
+  factory ArgumentsBindingGen.fPositional(X t1, [X t2]) {
+    return new ArgumentsBindingGen.positional(t1, t2);
+  }
+
+  namedArgumentsMethod(X t1, {X t2}) {}
+  positionalArgumentsMethod(X t1, [X t2]){}
+
+  set testSetter(X val) {}
+}
+
+main() {
+  // test functions
+  namedArgumentsFunc1(forgetType(t0Instance), t2: forgetType(t0Instance));
+  positionalArgumentsFunc1(forgetType(t0Instance), forgetType(t0Instance));
+
+  // test generic functions
+  namedArgumentsFunc2<S1>(forgetType(t0Instance), t2: forgetType(t0Instance));
+  positionalArgumentsFunc2<S1>(forgetType(t0Instance), forgetType(t0Instance));
+
+  // test class constructors
+  ArgumentsBindingClass instance1 =
+      new ArgumentsBindingClass(forgetType(t0Instance));
+  instance1 = new ArgumentsBindingClass.fNamed(forgetType(t0Instance),
+      t2: forgetType(t0Instance));
+  instance1 = new ArgumentsBindingClass.fPositional(forgetType(t0Instance),
+      forgetType(t0Instance));
+  instance1 = new ArgumentsBindingClass.named(forgetType(t0Instance),
+      t2: forgetType(t0Instance));
+  instance1 = new ArgumentsBindingClass.positional(forgetType(t0Instance),
+      forgetType(t0Instance));
+
+  // tests methods and setters
+  instance1.namedArgumentsMethod(forgetType(t0Instance),
+      t2: forgetType(t0Instance));
+  instance1.positionalArgumentsMethod(forgetType(t0Instance),
+      forgetType(t0Instance));
+  instance1.testSetter = forgetType(t0Instance);
+
+  // test static methods
+  ArgumentsBindingClass.namedArgumentsStaticMethod(forgetType(t0Instance),
+      t2: forgetType(t0Instance));
+  ArgumentsBindingClass.positionalArgumentsStaticMethod(
+      forgetType(t0Instance), forgetType(t0Instance));
+
+  // test generic class constructors
+  ArgumentsBindingGen<S1> instance2 =
+      new ArgumentsBindingGen(forgetType(t0Instance));
+  instance2 = new ArgumentsBindingGen.fNamed(forgetType(t0Instance),
+      t2: forgetType(t0Instance));
+  instance2 = new ArgumentsBindingGen.fPositional(forgetType(t0Instance),
+      forgetType(t0Instance));
+  instance2 = new ArgumentsBindingGen.named(forgetType(t0Instance),
+      t2: forgetType(t0Instance));
+  instance2 = new ArgumentsBindingGen.positional(forgetType(t0Instance),
+      forgetType(t0Instance));
+
+  // test generic class methods and setters
+  instance2.namedArgumentsMethod(
+      forgetType(t0Instance), t2: forgetType(t0Instance));
+  instance2.positionalArgumentsMethod(
+      forgetType(t0Instance), forgetType(t0Instance));
+  instance1.testSetter = forgetType(t0Instance);
+}
---
+++ LanguageFeatures/Subtyping/generated/right_FutureOr_return_value_A04_t01.dart
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion We say that a type T0 is a subtype of a type T1 (written T0 <: T1)
+ * when:
+ * Right FutureOr: T1 is FutureOr<S1> and
+ *   either T0 <: Future<S1>
+ *   or T0 <: S1
+ *   or T0 is X0 and X0 has bound S0 and S0 <: T1
+ *   or T0 is X0 & S0 and S0 <: T1
+ * @description Check that if a type T1 is S1 and a type T0 is X0 and X0 has
+ * bound S0 and S0 <: S1, then a type T0 is a subtype of a type T1.
+ * @author ngl@unipro.ru
+ */
+
+import "../utils/common.dart";
+
+class S1 {}
+class S0 extends S1 {
+}
+class X0 extends S0 {
+}
+
+X0 t0Instance = new X0();
+S1 t1Instance = new S1();
+
+
+
+
+S1 returnValueFunc() => forgetType(t0Instance);
+
+class ReturnValueTest {
+  static S1 staticTestMethod() => forgetType(t0Instance);
+
+  S1 testMethod() => forgetType(t0Instance);
+
+  S1 get testGetter => forgetType(t0Instance);
+}
+
+class ReturnValueGen<X> {
+  X testMethod() => forgetType(t0Instance);
+  X get testGetter => forgetType(t0Instance);
+}
+
+
+main() {
+  S1 returnValueLocalFunc() => forgetType(t0Instance);
+
+  returnValueFunc();
+  returnValueLocalFunc();
+
+  ReturnValueTest.staticTestMethod();
+
+  new ReturnValueTest().testMethod();
+  new ReturnValueTest().testGetter;
+
+  new ReturnValueGen<X0>().testMethod();
+  new ReturnValueGen<X0>().testGetter;
+}
