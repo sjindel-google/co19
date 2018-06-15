ec4a04f5178f8b8706c87d3f3f1841b23aec6b34
Natalya Golovleva <ngl@unipro.ru>
2018-06-15 18:25:29 +0700

    Tests fixed.

--- LanguageFeatures/Subtyping/generated/left_FutureOr_local_variable_A01_t01.dart
+++ LanguageFeatures/Subtyping/generated/left_FutureOr_local_variable_A01_t01.dart
@@ -18,21 +18,21 @@ import "dart:async";
 import "../utils/common.dart";
 
 class C1 {}
-class C0 extends C1 {}
-class S0 extends Future<C1> {
-  factory S0(FutureOr<C1> computation()) =>  computation();
-  asStream() => new Stream.fromFuture(new Completer().future);
-  catchError(Function onError, { bool test(Object error) }) => onError();
-  then<S0>(FutureOr<S0> onValue(C0 value), { Function onError }) =>
-      onValue(new C0());
-  timeout(Duration timeLimit, { FutureOr<C1> onTimeout() }) => onTimeout();
-  whenComplete(FutureOr action()) => action();
+class S0 implements Future<C1> {
+  // factory S0(FutureOr<C1> computation()) =>  computation();
+  asStream() => null;//new Stream.fromFuture(new Completer().future);
+  catchError(Function onError, { bool test(Object error) }) => null;//onError();
+  then<S0>(FutureOr<S0> onValue(C1 value), { Function onError }) => null;
+  //    onValue(new C1());
+  timeout(Duration timeLimit, { FutureOr<C1> onTimeout() }) => null;//onTimeout();
+  whenComplete(FutureOr action()) => null;//action();
 }
 
-FutureOr computation() async => new C1();
+//FutureOr computation() async => new C1();
+//FutureOr com() async => new S0();
+FutureOr<S0> t0Instance = new S0();
+Future<C1> t1Instance = new Future.value(new C1());
 
-FutureOr<S0> t0Instance = new S0(computation());
-Future<C0> t1Instance = new Future.value(new C0());
 
 
 
