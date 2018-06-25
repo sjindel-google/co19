c64be2141d1e4e9a883389d709ef6bf0477c1fcf
Natalya Golovleva <ngl@unipro.ru>
2018-06-25 21:38:26 +0700

    New tests added.

---
+++ LanguageFeatures/Subtyping/generated/left_FutureOr_fail_class_member_fail_A01_t03.dart
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion We say that a type T0 is a subtype of a type T1 (written T0 <: T1)
+ * when:
+ * Left FutureOr: T0 is FutureOr<S0>
+ *   and Future<S0> <: T1
+ *   and S0 <: T1
+ * @description Check that if a type T0 is FutureOr<S0> and Future<S0> is a
+ * subtype of T1 and S0 is not a subtype of a type T1, then a type T0 is not a
+ * subtype of a type T1.
+ * @author ngl@unipro.ru
+ */
+
+import "dart:async";
+import "../utils/common.dart";
+import "../../../Utils/expect.dart";
+
+class S0 {}
+
+FutureOr<S0> t0Instance = new S0();
+Future<S0> t1Instance = new Future.value(new S0());
+
+
+
+
+class ClassMemberSuper1_t03 {
+  Future<S0> m;
+
+  void set superSetter(Future<S0> val) {}
+}
+
+class ClassMember1_t03 extends Object with ClassMemberSuper1_t03 {
+
+  test1() {
+    m = forgetType(t0Instance);
+  }
+
+  test2() {
+    superSetter = forgetType(t0Instance);
+  }
+}
+
+class ClassMemberSuper2_t03<X> {
+  X m;
+
+  void set superSetter(X val) {}
+}
+
+class ClassMember2_t03<X> extends ClassMemberSuper2_t03<X> {
+
+  test1() {
+    m = forgetType(t0Instance);
+  }
+
+  test2() {
+    superSetter = forgetType(t0Instance);
+  }
+}
+
+main() {
+  Expect.throws(() {
+    new ClassMember1_t03().m = forgetType(t0Instance);
+  }, (e) => e is TypeError);
+  Expect.throws(() {
+    new ClassMember1_t03().superSetter = forgetType(t0Instance);
+  }, (e) => e is TypeError);
+  Expect.throws(() {
+    new ClassMember1_t03().test1();
+  }, (e) => e is TypeError);
+  Expect.throws(() {
+    new ClassMember1_t03().test2();
+  }, (e) => e is TypeError);
+
+  if (!isGenericFunctionType<Future<S0>>()) {
+    Expect.throws(() {
+    new ClassMember2_t03<Future<S0>>().m = forgetType(t0Instance);
+    }, (e) => e is TypeError);
+    Expect.throws(() {
+    new ClassMember2_t03<Future<S0>>().superSetter = forgetType(t0Instance);
+    }, (e) => e is TypeError);
+    Expect.throws(() {
+    new ClassMember2_t03<Future<S0>>().test1();
+    }, (e) => e is TypeError);
+    Expect.throws(() {
+    new ClassMember2_t03<Future<S0>>().test2();
+    }, (e) => e is TypeError);
+  }
+}
