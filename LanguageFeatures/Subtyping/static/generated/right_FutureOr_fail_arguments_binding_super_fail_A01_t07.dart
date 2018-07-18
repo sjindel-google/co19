5cc8ec5ce183c60808a7bdc4f4af9d506e1c57a9
Natalya Golovleva <ngl@unipro.ru>
2018-07-18 19:19:17 +0700

    New tests added.

---
+++ LanguageFeatures/Subtyping/static/generated/right_FutureOr_fail_arguments_binding_super_fail_A01_t07.dart
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion We say that a type T0 is a subtype of a type T1 (written T0 <: T1)
+ * when:
+ * Right FutureOr: T1 is FutureOr<S1> and
+ *   either T0 <: Future<S1>
+ *   or T0 <: S1
+ *   or T0 is X0 and X0 has bound S0 and S0 <: T1
+ *   or T0 is X0 & S0 and S0 <: T1
+ * @description Check that if a type T1 is FutureOr<S1> and a type T0 is not
+ * a subtype of types Future<S1> or S1, then a type T0 is not a subtype of a
+ * type T1.
+ * @author ngl@unipro.ru
+ */
+/**
+ * @description Check that if type T0 not a subtype of a type T1, then it cannot
+ * be used as an argument of type T1. Test superclass members. Super method
+ * required argument is tested.
+ * @author sgrekhov@unipro.ru
+ * @author ngl@unipro.ru
+ */
+/*
+ * This test is generated from right_FutureOr_fail_A01.dart and 
+ * arguments_binding_super_fail_x07.dart.
+ * Don't modify it. If you want to change this file, change one of the files 
+ * above and then run generator.dart to regenerate the tests.
+ */
+
+
+import '../../utils/common.dart';
+import '../../../../Utils/expect.dart';
+
+import "dart:async";
+
+class S1 {}
+class T0 {}
+
+T0 t0Instance = new T0();
+FutureOr<S1> t1Instance = new Future.value(new S1());
+
+
+
+
+class ArgumentsBindingSuper1_t02 {
+  ArgumentsBindingSuper1_t02(FutureOr<S1> value) {}
+
+  void superTest(FutureOr<S1> val) {}
+}
+
+class ArgumentsBinding1_t02 extends ArgumentsBindingSuper1_t02 {
+  ArgumentsBinding1_t02.valid() : super(null) {}
+}
+
+
+main() {
+  new ArgumentsBinding1_t02.valid().superTest(t0Instance);
+}
---
+++ LanguageFeatures/Subtyping/static/generated/right_FutureOr_fail_arguments_binding_super_fail_A02_t06.dart
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion We say that a type T0 is a subtype of a type T1 (written T0 <: T1)
+ * when:
+ * Right FutureOr: T1 is FutureOr<S1> and
+ *   either T0 <: Future<S1>
+ *   or T0 <: S1
+ *   or T0 is X0 and X0 has bound S0 and S0 <: T1
+ *   or T0 is X0 & S0 and S0 <: T1
+ * @description Check that if a type T1 is Future<S1> and a type T0 is X0 and
+ * X0 has bound S0 and S0 is not a subtype of Future<S1> or S1, then a type T0
+ * is not a subtype of a type T1.
+ * @author ngl@unipro.ru
+ */
+/**
+ * @description Check that if type T0 not a subtype of a type T1, then it cannot
+ * be used as an argument of type T1. Test superclass members. Super constructor
+ * required argument is tested.
+ * @author sgrekhov@unipro.ru
+ * @author ngl@unipro.ru
+ */
+/*
+ * This test is generated from right_FutureOr_fail_A02.dart and 
+ * arguments_binding_super_fail_x06.dart.
+ * Don't modify it. If you want to change this file, change one of the files 
+ * above and then run generator.dart to regenerate the tests.
+ */
+
+
+import '../../utils/common.dart';
+import '../../../../Utils/expect.dart';
+
+import "dart:async";
+
+class S1 {}
+class S0 {}
+class X0 extends S0 {
+}
+
+X0 t0Instance = new X0();
+FutureOr<S1> t1Instance = new Future.value(new S1());
+
+
+
+
+class ArgumentsBindingSuper1_t02 {
+  FutureOr<S1> m;
+  ArgumentsBindingSuper1_t02.short(this.m);
+}
+
+class ArgumentsBinding1_t02 extends ArgumentsBindingSuper1_t02 {
+  ArgumentsBinding1_t02.c5(X0 t1) : super.short(t1) {}
+}
+
+
+main() {
+  new ArgumentsBinding1_t02.c5(t0Instance);
+}
