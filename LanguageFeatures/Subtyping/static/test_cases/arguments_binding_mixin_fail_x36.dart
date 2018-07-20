8b76abc29a31ce1ca5345cd55c856cb1cbb4f5b1
Natalya Golovleva <ngl@unipro.ru>
2018-07-20 21:14:10 +0700

    New tests added.

---
+++ LanguageFeatures/Subtyping/static/test_cases/arguments_binding_mixin_fail_x36.dart
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @description Check that if type T0 not a subtype of a type T1, then it cannot
+ * be used as an argument of type T1. Test mixin members. Super setter argument
+ * is tested.
+ * @compile-error
+ * @author sgrekhov@unipro.ru
+ * @author ngl@unipro.ru
+ */
+
+class ArgumentsBindingSuper2_t03<X> {
+  void set superSetter(X val) {}
+}
+
+class ArgumentsBinding2_t03<X> extends Object with ArgumentsBindingSuper2_t03<X> {
+}
+
+main() {
+  new ArgumentsBinding2_t03<@T1>().superSetter = t0Instance;
+}
