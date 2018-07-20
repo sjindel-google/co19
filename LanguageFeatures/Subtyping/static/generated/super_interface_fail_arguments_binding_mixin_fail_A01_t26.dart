e2335c3ad6a7111b1dc10bd86b16edaf19111853
Natalya Golovleva <ngl@unipro.ru>
2018-07-20 18:38:45 +0700

    New tests added.

---
+++ LanguageFeatures/Subtyping/static/generated/super_interface_fail_arguments_binding_mixin_fail_A01_t26.dart
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion We say that a type T0 is a subtype of a type T1 (written T0 <: T1)
+ * when:
+ * Super-Interface: T0 is an interface type with super-interfaces S0,...Sn
+ * - and Si <: T1 for some i
+ * @description Check that if there is no i, such that Si <: T1, then T0 is not
+ * subtype of T1
+ * @author sgrekhov@unipro.ru
+ */
+/**
+ * @description Check that if type T0 not a subtype of a type T1, then it cannot
+ * be used as an argument of type T1. Test mixin members. Super getter return
+ * type is tested.
+ * @compile-error
+ * @author sgrekhov@unipro.ru
+ * @author ngl@unipro.ru
+ */
+/*
+ * This test is generated from super_interface_fail_A01.dart and 
+ * arguments_binding_mixin_fail_x26.dart.
+ * Don't modify it. If you want to change this file, change one of the files 
+ * above and then run generator.dart to regenerate the tests.
+ */
+
+
+import '../../utils/common.dart';
+import '../../../../Utils/expect.dart';
+
+class T1 {}
+
+// Missing subtype relation to T1
+abstract class S0 {}
+abstract class S1 {}
+abstract class S2 {}
+
+abstract class T0 implements S0, S1, S2  {}
+
+class T implements T0 {}
+
+T0 t0Instance = new T();
+T1 t1Instance = new T1();
+
+
+
+
+class ArgumentsBindingSuper1_t03 {
+  T1 get superGetter => t0Instance;
+}
+
+class ArgumentsBinding1_t03 extends Object with ArgumentsBindingSuper1_t03 {
+  test() {
+    superGetter;
+  }
+}
+
+main() {
+  new ArgumentsBinding1_t03().test();
+}
