eb7cc5ee1c29667d923cfbfbfe5229f64078765e
Natalya Golovleva <ngl@unipro.ru>
2018-07-24 21:32:42 +0700

    New tests added.

---
+++ LanguageFeatures/Subtyping/static/generated/left_FutureOr_fail_class_member_fail_super_A02_t07.dart
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion We say that a type T0 is a subtype of a type T1 (written T0 <: T1)
+ * when:
+ * Left FutureOr: T0 is FutureOr<S0>
+ *   and Future<S0> <: T1
+ *   and S0 <: T1
+ * @description Check that if a type T0 is FutureOr<S0> and S0 is a subtype of
+ * T1 but Future<S0> is not a subtype of T1 , then a type T0 is not a subtype of
+ * a type T1.
+ * @author sgrekhov@unipro.ru
+ */
+/**
+ * @description Check that if type T0 is not a subtype of a type T1, then
+ * instance of T0 cannot be be assigned to the superclass member of type T1.
+ * Assignment to instance variable of generic super class is tested.
+ * @author sgrekhov@unipro.ru
+ * @author ngl@unipro.ru
+ */
+/*
+ * This test is generated from left_FutureOr_fail_A02.dart and 
+ * class_member_fail_super_x07.dart.
+ * Don't modify it. If you want to change this file, change one of the files 
+ * above and then run generator.dart to regenerate the tests.
+ */
+
+
+import '../../utils/common.dart';
+import '../../../../Utils/expect.dart';
+
+import "dart:async";
+
+class T1 {}
+class S0 extends T1 {}
+
+FutureOr<S0> t0Instance = Future<S0>.value(new S0()); // Future<S0> is not a subtype of T1
+T1 t1Instance = new T1();
+
+
+
+
+class ClassMemberSuper2_t02<X> {
+  X m;
+
+  ClassMemberSuper2_t02() {
+  }
+}
+
+class ClassMember2_t02<X> extends ClassMemberSuper2_t02<X> {
+  ClassMember2_t02() : super() {}
+  
+  ClassMember2_t02.valid() : super();
+}
+
+main() {
+  new ClassMember2_t02<T1>.valid().m = t0Instance;
+}
---
+++ LanguageFeatures/Subtyping/static/generated/super_interface_fail_class_member_fail_super_A01_t08.dart
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion We say that a type T0 is a subtype of a type T1 (written T0 <: T1)
+ * when:
+ * Super-Interface: T0 is an interface type with super-interfaces S0,...Sn
+ * - and Si <: T1 for some i
+ * @description Check that if there is no i, such that Si <: T1, then T0 is not
+ * subtype of T1
+ * @author sgrekhov@unipro.ru
+ */
+/**
+ * @description Check that if type T0 is not a subtype of a type T1, then
+ * instance of T0 cannot be be assigned to the superclass member of type T1.
+ * Assignment to instance variable of generic super class is tested.
+ * @author sgrekhov@unipro.ru
+ * @author ngl@unipro.ru
+ */
+/*
+ * This test is generated from super_interface_fail_A01.dart and 
+ * class_member_fail_super_x08.dart.
+ * Don't modify it. If you want to change this file, change one of the files 
+ * above and then run generator.dart to regenerate the tests.
+ */
+
+
+import '../../utils/common.dart';
+import '../../../../Utils/expect.dart';
+
+class T1 {}
+
+// Missing subtype relation to T1
+abstract class S0 {}
+abstract class S1 {}
+abstract class S2 {}
+
+abstract class T0 implements S0, S1, S2  {}
+
+class T implements T0 {}
+
+T0 t0Instance = new T();
+T1 t1Instance = new T1();
+
+
+
+
+class ClassMemberSuper2_t02<X> {
+  X m;
+
+  ClassMemberSuper2_t02() {}
+
+  void set superSetter(X val) {}
+}
+
+class ClassMember2_t02<X> extends ClassMemberSuper2_t02<X> {
+  ClassMember2_t02() : super() {}
+
+  ClassMember2_t02.valid() : super();
+}
+
+main() {
+  new ClassMember2_t02<T1>.valid().superSetter = t0Instance;
+}
