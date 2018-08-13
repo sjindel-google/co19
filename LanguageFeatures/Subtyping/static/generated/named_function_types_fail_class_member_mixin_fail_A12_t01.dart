ebda71e5e3eef3fab59e018e2f66b99689d43d68
Natalya Golovleva <ngl@unipro.ru>
2018-08-13 18:58:36 +0700

    Subtyping. 4 tests_cases itegrated to one.

--- LanguageFeatures/Subtyping/static/generated/named_function_types_fail_class_member_mixin_fail_A12_t01.dart
+++ LanguageFeatures/Subtyping/static/generated/named_function_types_fail_class_member_mixin_fail_A12_t01.dart
@@ -71,12 +71,22 @@ T1 t1Instance = t1Func;
 
 class ClassMemberSuper1_t03 {
   T1 m;
+  void set superSetter(T1 val) {} //# 02: compile-time error
 }
 
 class ClassMember1_t03 extends Object with ClassMemberSuper1_t03 {
+  test1() {
+    m = t0Instance; //# 03: compile-time error
+  }
+  test2() {
+    superSetter = t0Instance; //# 04: compile-time error
+  }
 }
 
 main() {
-  new ClassMember1_t03().m = t0Instance;
+  new ClassMember1_t03().m = t0Instance; //# 01: compile-time error
+  new ClassMember1_t03().superSetter = t0Instance;  //# 02: compile-time error
+  new ClassMember1_t03().test1();  //# 03: compile-time error
+  new ClassMember1_t03().test2();  //# 04: compile-time error
 }
 
