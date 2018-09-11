2f8d27ecf793bfab113940e65bd942d730c494a0
Natalya Golovleva <ngl@unipro.ru>
2018-09-11 19:10:33 +0700

    int-to-double. Test fixed due issue #160.

--- LanguageFeatures/int-to-double/representation_t01.dart
+++ LanguageFeatures/int-to-double/representation_t01.dart
@@ -13,19 +13,20 @@
  * value of a double valued integer literal can be represented exactly as an
  * IEEE 754 double-precision value. The unbounded integer 18446744073709551614
  * has the exact double value represented as 0x43F0000000000000.
+ * (Incorrect number is specified in tested documentation.)
  * @author ngl@unipro.ru
  */
 
-double d1 = 18446744073709551614;
+double d1 = 18446744073709551616;
 
 class C {
-  static double d2 = 18446744073709551614;
+  static double d2 = 18446744073709551616;
 
-  double d3 = 18446744073709551614;
+  double d3 = 18446744073709551616;
 }
 
 main() {
-  double d4 = 18446744073709551614;
+  double d4 = 18446744073709551616;
 
   new C();
 }
