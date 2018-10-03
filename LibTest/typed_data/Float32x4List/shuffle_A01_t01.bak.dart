30786d586d0b4bc8d087ace89135f490bab14bce
Sergey G. Grekhov <sgrekhov@unipro.ru>
2018-10-03 17:21:42 +0700

    Additional fix for #162. Fix some other lists

--- LibTest/typed_data/Float32x4List/shuffle_A01_t01.bak.dart
+++ LibTest/typed_data/Float32x4List/shuffle_A01_t01.bak.dart
@@ -8,9 +8,10 @@
  * Shuffles the elements of this list randomly.
  * @description Checks that [this] is shuffled randomly.
  * @author ngl@unipro.ru
- * @author sgrekhov@unipro.ru
  */
+
 import "dart:typed_data";
+import "dart:math";
 import "../../../Utils/expect.dart";
 
 pack(x, y, z, w) => new Float32x4(x, y, z, w);
@@ -20,37 +21,38 @@ equal(obj1, obj2) {
   return res.flagX && res.flagY && res.flagZ && res.flagW;
 }
 
-bool isOrderChanged(List<Float32x4> list, Float32x4List sl) {
+check(List<Float32x4> list, Float32x4List sl) {
   Expect.equals(list.length, sl.length);
-  bool found = false;
-  bool moved = false;
+  bool p1 = false;
+  bool p2 = true;
   for (int i = 0; i < list.length; i++) {
     for (int j = 0; j < list.length; j++) {
-      if (equal(list[i], sl[j])) {
-        found = true;
-        if (i != j) moved = true;
+      if (equal(list[i], sl[j]))  {
+        p1 = true;
+        if (i != j) p2 = false;
         break;
       }
     }
-    Expect.isTrue(found, "Element ${list[i]} not found in shuffled list");
+    Expect.isTrue(p1, "Element ${list[i]} is not in sl");
   }
-  return moved;
+  Expect.isFalse(p2, "sl wasn't shuffled");
 }
 
 main() {
-  List<Float32x4> list = [
+  var list = [
     pack(1.0, 2.0, 3.0, 4.0), pack(2.0, 3.0, 4.0, 5.0),
     pack(3.0, 4.0, 5.0, 6.0), pack(4.0, 5.0, 6.0, 7.0),
     pack(5.0, 6.0, 7.0, 8.0)
   ];
-  var counter = 0;
-  for (int i = 0; i < 10; i++) {
-    var sl = new Float32x4List.fromList(list);
-    sl.shuffle();
-    if (!isOrderChanged(list, sl)) {
-      counter++;
-    }
-  }
-  /* We allow a couple of shuffles return data in the same order */
-  Expect.isTrue(counter < 3);
+  var sl = new Float32x4List.fromList(list);
+  sl.shuffle();
+  check(list, sl);
+
+  sl = new Float32x4List.fromList(list);
+  sl.shuffle(new Random());
+  check(list, sl);
+
+  sl = new Float32x4List.fromList(list);
+  sl.shuffle(new Random(6));
+  check(list, sl);
 }
